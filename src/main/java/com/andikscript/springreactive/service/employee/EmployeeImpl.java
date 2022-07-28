836ff43b4f47faec63a26bca9782ee516ce7493f
andikscript <andikscript@gmail.com>
2022-07-28 23:18:57 +0700

    add annotation

--- src/main/java/com/andikscript/springreactive/service/employee/EmployeeImpl.java
+++ src/main/java/com/andikscript/springreactive/service/employee/EmployeeImpl.java
@@ -2,8 +2,10 @@ package com.andikscript.springreactive.service.employee;
 
 import com.andikscript.springreactive.model.Employee;
 import com.andikscript.springreactive.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+@Service
 public class EmployeeImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
